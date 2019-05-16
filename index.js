6b55e7e2cfbd90adae4f14be1a8a2dd4b0a86b84
Jesse Van Volkinburg <vanvoljg@gmail.com>
2019-05-16 13:39:56 -0700

    loop.js file gets created

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 'use strict';
 
+const fs = require('fs');
+
+// Code to write into file:
+const insertCode = `'use strict';
+let names = ['John Paul', 'John George', 'John Rob'];
+names.forEach( (name) => { console.log(name); });
+`;
+
+// Procedure:
+// Iterate over each string, get byte for each character, push the bytes one by one to a new buffer
+const loopBuffer = Buffer.alloc(insertCode.length);
+
+for (let i = 0; i < insertCode.length; i++) {
+  loopBuffer[i] = insertCode.charCodeAt(i);
+}
+
+console.log(loopBuffer);
+
+fs.writeFile('./files/loop.js', loopBuffer, (err) => {
+  if (err) throw err;
+  console.log('wrote file');
+});
