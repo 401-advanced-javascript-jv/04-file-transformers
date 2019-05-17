4411b5ca140e7698b307a581a6d59a37de55af8f
Jesse Van Volkinburg <vanvoljg@gmail.com>
2019-05-16 18:05:47 -0700

    cleaned up loop code

--- index-loop.js
+++ index-loop.js
@@ -2,7 +2,7 @@
 
 const fs = require('fs').promises;
 
-// Code to write into file:
+// Code snippet to write into file:
 const insertCode = `'use strict';
 let names = ['John Paul', 'John George', 'John Rob'];
 names.forEach( (name) => { console.log(name); });
@@ -12,12 +12,12 @@ names.forEach( (name) => { console.log(name); });
 // Iterate over each string, get byte for each character, push the bytes one by one to a new buffer
 let loopBuffer = Buffer.alloc(0);
 
-for (let i = 0; i < insertCode.length; i++) {
-  let charBuffer = Buffer.alloc(1);
-  charBuffer[0] = insertCode.charCodeAt(i);
+for (let char of insertCode) {
+  let charBuffer = Buffer.alloc(1, char.charCodeAt());
   loopBuffer = Buffer.concat([loopBuffer, charBuffer]);
 }
 
+// Write the file
 fs.writeFile('./files/loop.js', loopBuffer)
   .then(() => { console.log('wrote file'); })
   .catch((err) => { throw err; });
